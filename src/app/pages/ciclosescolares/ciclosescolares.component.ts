feab4d8ecdd26782bf0d218a1060acb300bb1b1a
avidaurri <40777508+avidaurri@users.noreply.github.com>
2020-02-25 01:08:32 -0600

    primer commit

---
+++ src/app/pages/ciclosescolares/ciclosescolares.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclosescolares',
+  templateUrl: './ciclosescolares.component.html',
+  styles: []
+})
+export class CiclosescolaresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
